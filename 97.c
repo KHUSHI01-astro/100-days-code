93eac0d7f25b4e9017c82ae86f9aa69747aff4f7
KHUSHI01-astro <khusiydv48@gmail.com>
2025-10-14 11:25:47 +0530

    Create 97.c

---
+++ 97.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char name[1000];
+    int i = 0;
+
+    printf("Enter full name: ");
+    fgets(name, sizeof(name), stdin);
+
+    if (name[0] != ' ' && name[0] != '\n') {
+        printf("%c.", name[0]);
+    }
+
+    while (name[i] != '\0' && name[i] != '\n') {
+        if (name[i] == ' ' && name[i+1] != ' ' && name[i+1] != '\0' && name[i+1] != '\n') {
+            printf("%c.", name[i+1]);
+        }
+        i++;
+    }
+
+    printf("\n");
+    return 0;
+}
